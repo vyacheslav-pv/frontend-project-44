9a85d6dccd178e932476c31bfb0484283d46cdb0
Vyacheslav Pilyugin <vyach-pvv@yandex.ru>
2022-09-19 23:29:58 +0700

    add cli.js

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+const askName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+export default askName;
